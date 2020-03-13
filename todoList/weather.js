3b7a90b9454e80c79ff506fb30100d184aa2ab9d
sungchul-dev <devnori@bigin.io>
2020-03-14 04:12:50 +0900

    [ADD] 바닐라 JS ToDoList 완성

---
+++ todoList/weather.js
@@ -0,0 +1,39 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        console.log(parsedCoords);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
